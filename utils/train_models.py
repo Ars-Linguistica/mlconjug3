0ed022152dede22ab96cdbeebdebb8000b32b2d2
SekouD <sekoud.python@gmail.com>
2019-06-29 13:33:28 +0000

    Added timer and changed split_proportion to 0.9.

--- utils/train_models.py
+++ utils/train_models.py
@@ -15,16 +15,18 @@ train_model.
 import mlconjug
 import pickle
 import json
-import pprint
+from pprint import pprint
 from functools import partial
 from collections import defaultdict
-from tabulate import tabulate
+# from tabulate import tabulate
+from time import time
 
 # Set a language to train the Conjugator on
 langs = ('en', 'es', 'fr', 'it', 'pt', 'ro')
 
 managers = (mlconjug.Verbiste, mlconjug.ConjugManager)
 results = defaultdict(dict)
+start = time()
 
 for lang in langs:
     for manager in managers:
@@ -40,7 +42,7 @@ for lang in langs:
 
         # Feature reduction
         feature_reductor = mlconjug.SelectFromModel(mlconjug.LinearSVC(penalty="l1",
-                                                                       max_iter=24000,
+                                                                       max_iter=36000,
                                                                        dual=False,
                                                                        verbose=0))
 
@@ -56,27 +58,32 @@ for lang in langs:
         # Initialize Data Set
         dataset = mlconjug.DataSet(manager(language=lang).verbs)
         dataset.construct_dict_conjug()
-        dataset.split_data(proportion=0.95)
+        dataset.split_data(proportion=0.90)
 
         # Initialize Conjugator
         model = mlconjug.Model(vectorizer, feature_reductor, classifier)
         conjugator = mlconjug.Conjugator(lang, model)
 
         # Training and prediction
+        model_start = time()
         conjugator.model.train(dataset.train_input, dataset.train_labels)
         predicted = conjugator.model.predict(dataset.test_input)
+        model_duration = round(time() - model_start, 3)
 
         # Assess the performance of the model's predictions
         score = len([a == b for a, b in zip(predicted, dataset.test_labels) if a == b]) / len(predicted)
-        print('The score of the {0} model is {1} with the {2} model.'.format(lang, score, manager.__name__))
+        # print('The score of the {0} model is {1} with the {2} model.\nThe training took {3} seconds.'.format(lang, score, manager.__name__, duration))
 
-        results[lang][manager] = score
+        results[lang][manager.__name__] = {'language': lang, 'manager': manager.__name__, 'score': score, 'duration': model_duration}
+        pprint(results[lang][manager.__name__])
 
         # Save trained model
-        with open('/home/ubuntu/Documents/mlconjug/utils/raw_data/experiments/trained_model-{0}- {1}.pickle'.format(lang, manager), 'wb') as file:
+        with open('/home/ubuntu/PycharmProjects/mlconjug/utils/raw_data/experiments/trained_model-{0}- {1}.pickle'.format(lang, manager), 'wb') as file:
             pickle.dump(conjugator.model, file)
 
         # Save trained model
-        with open('/home/ubuntu/Documents/mlconjug/utils/raw_data/experiments/results.json', 'w', encoding='utf-8') as file:
-            json.dump(conjugator.model, file, ensure_ascii=False, indent=4)
+        with open('/home/ubuntu/PycharmProjects/mlconjug/utils/raw_data/experiments/results.json', 'w', encoding='utf-8') as file:
+            json.dump(results, file, ensure_ascii=False, indent=4)
+duration = round(time() - start, 3)
+print('The training took {0} seconds in total.'.format(duration))
 pprint(results)
