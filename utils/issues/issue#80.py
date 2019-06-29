9837af1d7ad7f8339ba707d31fb6523704fb8021
SekouD <sekoud.python@gmail.com>
2019-06-29 21:12:05 +0000

    Script to re[roduce issue #80.

---
+++ utils/issues/issue#80.py
@@ -0,0 +1,26 @@
+# steps to reproduce Github issue #80.
+#  Example: 'awake' has 'reaw:aken' as a template name to reconstruct the conjugated forms.
+#  Implement ranking of verbs in each category for representativity of the conjugation paradigm.
+
+
+import mlconjug
+
+verbList = ['arise', 'awake', 'be', 'bear', 'beat', 'become', 'begin', 'bend', 'bet', 'bind', 'bite', 'bleed', 'blow',
+            'break', 'breed', 'bring', 'broadcast', 'build', 'burn', 'burst', 'buy', 'be', 'catch', 'choose', 'clung',
+            'come', 'cost', 'creep', 'cut', 'delt', 'dig', 'do', 'dream', 'drunk', 'drive', 'eat', 'fall', 'feed',
+            'feel', 'fight', 'find', 'fly', 'forbid', 'forget', 'forgive', 'freeze', 'get', 'give', 'go', 'ground',
+            'grow', 'hang', 'have', 'hear', 'hide', 'hit', 'hold', 'hurt', 'keep', 'knelt', 'know', 'lay', 'lead',
+            'learn', 'leave', 'lend', 'lie', 'lie', 'light', 'lose', 'make', 'ment', 'meet', 'mow', 'overtaken', 'pay',
+            'put', 'read', 'ride', 'ring', 'rise', 'run', 'sawn', 'say', 'see', 'sell', 'send', 'set', 'sew', 'shake',
+            'shed', 'shine', 'shoot', 'show', 'shrink', 'shut', 'sing', 'sink', 'sit', 'sleep', 'slide', 'smell', 'sow',
+            'speak', 'spell', 'spend', 'spill', 'spat', 'spread', 'stand', 'steal', 'stick', 'sting', 'stunk', 'strike',
+            'swear', 'sweep', 'swell', 'swum', 'swing', 'take', 'teach', 'tear', 'tell', 'think', 'throw', 'understand',
+            'wake', 'wear', 'weep', 'win', 'wound', 'write']
+
+default_conjugator = mlconjug.Conjugator(language='en')
+mlOutput = []
+for item in verbList:
+    pp = default_conjugator.conjugate(item).conjug_info
+    mlOutput.append(pp)
+
+print('Done')
