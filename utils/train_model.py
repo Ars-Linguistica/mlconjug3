9d522427c5e69935bb32d02c12f7a5b736a4a31e
SekouD <sekou.diao@gmail.com>
2019-06-29 11:22:49 +0000

    Skeleton for training new models.

---
+++ utils/train_model.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+train_model.
+
+| Trains a collection of new conjugation models.
+
+
+| The conjugation data conforms to the JSON structure of the files in mlconjug/data/conjug_manager/
+| or to the XML schema defined by Verbiste.
+| More information on Verbiste at https://perso.b2b2c.ca/~sarrazip/dev/conjug_manager.html
+
+"""
