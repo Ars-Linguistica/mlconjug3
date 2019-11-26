3ea59e39c0cf3d770f39138a0b5d7bd1beeca5ab
SekouD <sekoud.python@gmail.com>
2019-11-26 17:47:10 +0100

    Added global random seed.

--- utils/evaluate_best_models.py
+++ utils/evaluate_best_models.py
@@ -17,7 +17,9 @@ except ImportError:
 
 
 # langs = ('ro', 'it', 'en', 'es', 'fr', 'pt')
-langs =  ('ro', 'fr')
+langs = ('ro', 'fr')
+
+results = []
 
 for lang in langs:
     with open('raw_data/models/best_model_{0}.pkl'.format(lang), 'rb') as file:
@@ -35,10 +37,15 @@ for lang in langs:
     predicted2 = best_model.predict(dataset.verbs_list)
 
     score = len([a == b for a, b in zip(predicted, dataset.test_labels) if a == b]) / len(predicted)
+    misses = len([a != b for a, b in zip(predicted, dataset.test_labels) if a != b])
+    entries = len(predicted)
     print('The score of the {0} model trained on the train set is {1} on test set.'.format(lang, score))
 
     score2 = len([a == b for a, b in zip(predicted2, dataset.templates_list) if a == b]) / len(predicted2)
+    misses2 = len([a != b for a, b in zip(predicted2, dataset.templates_list) if a != b])
+    entries2 = len(predicted2)
     print('The score of the {0} model trained on the train set is {1} on full dataset.'.format(lang, score2))
     print('ok')
+    results.append((lang, best_model, best_params, (score, misses, entries), (score2, misses2, entries2)))
 
 print('ok')
