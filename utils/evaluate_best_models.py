44a7ac850f0d3d85e785a3f8842d5879794c4270
SekouD <sekoud.python@gmail.com>
2019-11-28 20:42:42 +0100

    cleanup.

--- utils/evaluate_best_models.py
+++ utils/evaluate_best_models.py
@@ -1,15 +1,7 @@
-import logging
-import sys
-import os
-import warnings
-from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
+
 import mlconjug
 import joblib
-from pprint import pprint
-from functools import partial
-from time import time
-import datetime
+
 try:
     from pathlib import Path
 except ImportError:
@@ -18,7 +10,7 @@ except ImportError:
 
 langs = ('ro', 'it', 'en', 'es', 'fr', 'pt')
 
-results = []
+results = {}
 
 for lang in langs:
     my_file = Path('raw_data/models/reduced_search/best_model_{0}.pkl'.format(lang))
@@ -28,7 +20,7 @@ for lang in langs:
         with open('raw_data/models/reduced_search/best_model_{0}.pkl'.format(lang), 'rb') as file:
             best_model = joblib.load(file)
 
-        # Save best model parameters
+        # Load best model parameters
         with open('raw_data/experiments/reduced_search/best_model_parameters_{0}.pkl'.format(lang),
                   'rb') as file:
             best_params = joblib.load(file)
@@ -42,13 +34,12 @@ for lang in langs:
         score = len([(a, b) for a, b in zip(predicted, dataset.test_labels) if a == b]) / len(predicted)
         misses = len([(a, b) for a, b in zip(predicted, dataset.test_labels) if a != b])
         entries = len(predicted)
-        print('The score of the {0} model trained on the train set is {1} on test set.'.format(lang, score))
+        print('The score of the {0} best model is {1} on test set.'.format(lang, score))
 
         score2 = len([(a, b) for a, b in zip(predicted2, dataset.templates_list) if a == b]) / len(predicted2)
         misses2 = len([(a, b) for a, b in zip(predicted2, dataset.templates_list) if a != b])
         entries2 = len(predicted2)
-        print('The score of the {0} model trained on the train set is {1} on full dataset.'.format(lang, score2))
-        print('ok')
-        results.append((lang, best_model, best_params, (score, misses, entries), (score2, misses2, entries2)))
+        print('The score of the {0} best model is {1} on full dataset.\n'.format(lang, score2))
+        results[lang] = (lang, best_model, best_params, (score, misses, entries), (score2, misses2, entries2))
 
 print('ok')
