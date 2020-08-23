1dc5e92dc448b1fef7afa4741159d91717992b5a
SekouDiaoNlp <diao.sekou.nlp@gmail.com>
2020-08-23 19:59:18 +0200

    Train only on full dataset.

--- utils/quick_training_with_default_parameters.py
+++ utils/quick_training_with_default_parameters.py
@@ -61,13 +61,13 @@ for lang in langs:
     conjugator = mlconjug3.Conjugator(lang, model)
 
     # Training and prediction
-    print('training {0} model on train set'.format(lang))
-    t0 = time()
-    conjugator.model.train(dataset.train_input, dataset.train_labels)
-    duration = round(time() - t0, 3)
-    print('{0} model trained on train set in {1} seconds.'.format(lang, duration))
-    predicted = conjugator.model.predict(dataset.test_input)
-    predicted2 = conjugator.model.predict(dataset.verbs_list)
+    # print('training {0} model on train set'.format(lang))
+    # t0 = time()
+    # conjugator.model.train(dataset.train_input, dataset.train_labels)
+    # duration = round(time() - t0, 3)
+    # print('{0} model trained on train set in {1} seconds.'.format(lang, duration))
+    # predicted = conjugator.model.predict(dataset.test_input)
+    # predicted2 = conjugator.model.predict(dataset.verbs_list)
 
     print('training {0} model on full data set'.format(lang))
     t0 = time()
@@ -77,22 +77,22 @@ for lang in langs:
     predicted_full = conjugator.model.predict(dataset.verbs_list)
 
     # Assess the performance of the model's predictions
-    score = len([a == b for a, b in zip(predicted, dataset.test_labels) if a == b]) / len(predicted)
-    misses = len([a != b for a, b in zip(predicted, dataset.test_labels) if a != b])
-    entries = len(predicted)
-    print('The score of the {0} model trained on the train set is {1} with the {2} model on test set.'.format(lang, score, managers[0].__name__))
-
-    score2 = len([a == b for a, b in zip(predicted2, dataset.templates_list) if a == b]) / len(predicted2)
-    misses2 = len([a == b for a, b in zip(predicted2, dataset.templates_list) if a != b])
-    entries2 = len(predicted2)
-    print('The score of the {0} model trained on the train set is {1} with the {2} model on full dataset.'.format(lang, score2, managers[0].__name__))
+    # score = len([a == b for a, b in zip(predicted, dataset.test_labels) if a == b]) / len(predicted)
+    # misses = len([a != b for a, b in zip(predicted, dataset.test_labels) if a != b])
+    # entries = len(predicted)
+    # print('The score of the {0} model trained on the train set is {1} with the {2} model on test set.'.format(lang, score, managers[0].__name__))
+    #
+    # score2 = len([a == b for a, b in zip(predicted2, dataset.templates_list) if a == b]) / len(predicted2)
+    # misses2 = len([a == b for a, b in zip(predicted2, dataset.templates_list) if a != b])
+    # entries2 = len(predicted2)
+    # print('The score of the {0} model trained on the train set is {1} with the {2} model on full dataset.'.format(lang, score2, managers[0].__name__))
 
     score_full = len([a == b for a, b in zip(predicted_full, dataset.templates_list) if a == b]) / len(predicted_full)
     misses_full = len([a == b for a, b in zip(predicted_full, dataset.templates_list) if a != b])
     entries_full = len(predicted_full)
     print('The score of the {0} model trained on the full dataset is {1} with the {2} model.'.format(lang, score_full, managers[0].__name__))
 
-    results[lang] = (duration, duration2), (score, misses, entries), (score2, misses2, entries2), (score_full, misses_full,entries_full)
+    results[lang] = duration2, (score_full, misses_full, entries_full)
 
     # Save trained model
     with open('raw_data/experiments/trained_model-{0}-final.pickle'.format(lang), 'wb') as file:
