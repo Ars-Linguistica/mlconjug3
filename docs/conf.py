1a7fd155236cfe0c7de4b52dcac134026e6a2e99
SekouDiaoNlp <diao.sekou.nlp@gmail.com>
2021-04-18 09:12:05 +0200

    Updated the year wherever it was still 2020.

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'MLConjug3'
 author = u'SekouDiaoNlp'
-copyright = u"2020, SekouDiaoNlp"
+copyright = u"2021, SekouDiaoNlp"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
