ed1519c79bd3d4d614ec54594e73ae9e7a0b1018
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-15 09:51:41 +0100

    Update TUI4.py

--- mlconjug3/TUI/TUI4.py
+++ mlconjug3/TUI/TUI4.py
@@ -62,20 +62,6 @@ class TUI:
                     self.conjugation_tables.clear()
                     self.conjugation_tables.add(textual.Text("The verb is not in the dataset. The conjugation was not possible."))
 
-    def handle_export(self):
-        """
-        Handles the export button click event.
-        Exports the conjugation tables to a json file.
-        """
-        verb_conjugations = {}
-        for i in range(self.verb_history.count()):
-            verb = self.verb_history.get_item(i)
-            conjugation_result = self.conjugator.conjugate(verb, self.subject_selector.get_value())
-            verb_conjugations[verb] = conjugation_result
-        with open("conjugations.json", "w") as file:
-            json.dump(verb_conjugations, file)
-        self.app.show_message("Conjugations exported to conjugations.json")
-
     def handle_history_select(self, verb):
         """
         Handles the verb history list select event.
@@ -94,6 +80,85 @@ class TUI:
         else:
             self.conjugation_tables.clear()
             self.conjugation_tables.add(textual.Text("The verb is not in the dataset. The conjugation was not possible."))
+        
+    def handle_export(self):
+        conjugations = {}
+        for item in self.verb_history.get_items():
+            conjugation_result = self.conjugator.conjugate(item, self.subject_selector.get_value())
+            conjugations[item] = conjugation_result
+        filename = self.prompt.get_value() + '.json'
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(conjugations, file, ensure_ascii=False, indent=4)
+        self.app.alert("Conjugations exported to " + filename)
+
+    def handle_language_select(self, language):
+        self.conjugator.set_language(language)
+        self.prompt.set_value('')
+        self.conjugation_tables.clear()
+        self.sample_verbs.clear()
+
+    def handle_subject_select(self, subject):
+        self.handle_submit(self.prompt.get_value())
+        self.app.focus(self.subject_selector)
+    
+    def handle_similar_verb_select(self, verb):
+        self.prompt.set_value(verb)
+        self.handle_submit(verb)
+        self.app.focus(self.prompt)
+        
+    def handle_export(self):
+        export_format = textual.dialog.ask("What format do you want to export?", ["JSON", "CSV"])
+        if export_format:
+            export_path = textual.dialog.ask("Where do you want to save the file?", "file")
+        if export_path:
+        with open(export_path, 'w', encoding='utf-8') as file:
+            json.dump(self.conjugation_tables, file, ensure_ascii=False, indent=4)
+            textual.alert("The conjugations have been succesfully saved to {0}.".format(export_path))
+            self.app.focus(self.export_button)
+            
+    def display_conjugations_in_table(self, conjugations):
+        self.conjugation_tables.clear()
+        headers = ["Person", "Singular", "Plural"]
+        rows = []
+        for person, conjugation in conjugations.items():
+            rows.append([person, conjugation["singular"], conjugation["plural"]])
+        table = textual.Table(headers=headers, rows=rows)
+        self.conjugation_tables.add(table)
+    
+    def change_verb_tense(self):
+        self.tense_selector = self.right_section.add(textual.Select(options=["present", "past", "future", "subjunctive", "conditional"]))
+        self.tense_selector.on_select(self.handle_tense_change)
+
+    def handle_tense_change(self, selection):
+        self.tense = selection
+        self.handle_submit(self.verb_input.value)
+    
+    def compare_multiple_conjugations(self):
+        self.multiple_verb_input = self.left_section.add(textual.Prompt(placeholder="Enter multiple verbs separated by commas"))
+        self.multiple_verb_input.on_submit(self.handle_multiple_submit)
+
+    def handle_multiple_submit(self, input_text):
+        verbs = input_text.split(',')
+        results = {}
+            for verb in verbs:
+            result = self.conjugator.conjugate(verb.strip(), self.subject)
+            results[verb.strip()] = result.conjug_info
+        self.conjugation_tables.clear()
+        for verb, conjugations in results.items():
+            verb_table = self.conjugation_tables.add(textual.Table(flex=1))
+            verb_table.add_column("Tense/Person", flex=1)
+            verb_table.add_column(verb, flex=1)
+            for tense, conjugation in conjugations.items():
+                verb_table.add_row(tense, conjugation)
+        self.verb_history.append(input_text)
+    
+    
+
+    
+        
+    
+
+
 
 
     
