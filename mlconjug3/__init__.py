39b2573ce1abb489a510729df88df9f4e2171bbf
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-16 03:36:05 +0100

    Update __init__.py

--- mlconjug3/__init__.py
+++ mlconjug3/__init__.py
@@ -55,6 +55,13 @@ import inspect
 
 extract_verb_features = VerbFeatures
 
+# Sets up the automatic translation of annotated strings displayed to the user.
+_TRANSLATIONS_PATH = pkg_resources.resource_filename(_RESOURCE_PACKAGE, 'locale')
+
+_SUPPORTED_LANGUAGES = ('default', 'en', 'es', 'fr', 'it', 'pt', 'ro')
+_TRANSLATED_LANGUAGES = _SUPPORTED_LANGUAGES[2:]
+
+
 def _get_user_locale():
     """
     | Gets the user locale to set the user interface language language.
