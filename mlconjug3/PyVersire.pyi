431f3bd28b80e3e76194987c1972fc12519dbc20
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-16 06:19:57 +0100

    Create PyVersire.pyi

---
+++ mlconjug3/PyVersire.pyi
@@ -0,0 +1,13 @@
+class Verbiste(ConjugManager):
+    def _load_verbs(self, verbs_file:str)->None:
+        pass
+
+    @staticmethod
+    def _parse_verbs(file:str)->OrderedDict:
+        pass
+
+    def _load_conjugations(self, conjugations_file:str)->None:
+        pass
+
+    def _parse_conjugations(self, file:str)->OrderedDict:
+        pass
