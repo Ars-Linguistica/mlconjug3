28e5340dec32c6a2020b56ab3927eca29704195b
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-16 17:38:24 +0100

    Create validator.py

---
+++ mlconjug3/validator.py
@@ -0,0 +1,16 @@
+class Validation:
+    def __init__(self, language='en'):
+        self.language = language
+
+    def validate(self, verb, conjugation):
+        """
+        Validate the conjugation of the given verb.
+
+        :param verb: str.
+            The verb to be validated.
+        :param conjugation: str.
+            The conjugation of the verb.
+        :return: bool.
+            True if the conjugation is valid, False otherwise.
+        """
+        pass
