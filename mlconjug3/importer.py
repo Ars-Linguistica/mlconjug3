4ec607fc2e4d1eae2af7b08e6ee23230af1a927c
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-16 17:58:06 +0100

    Create importer.py

---
+++ mlconjug3/importer.py
@@ -0,0 +1,7 @@
+class ConjugationImporter:
+    """
+    A class that imports verbs from external sources such as a spreadsheet.
+    """
+    def __init__(self, filename, format='csv'):
+        """
+       
