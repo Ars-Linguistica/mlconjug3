06ebd1c84a0459cc1d10dc419f9cd2d983fbf551
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-16 17:37:29 +0100

    Create verb_corrector.py

---
+++ mlconjug3/verb_corrector.py
@@ -0,0 +1,16 @@
+class VerbCorrector:
+    def __init__(self, language='en'):
+        self.language = language
+
+    def correct(self, verb, conjugation):
+        """
+        Correct the conjugation of the given verb.
+
+        :param verb: str.
+            The verb to be corrected.
+        :param conjugation: str.
+            The conjugation of the verb.
+        :return: str.
+            The corrected conjugation of the verb.
+        """
+        pass
