647cef268469f1face9cc0e2544b683e4e0a107e
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-18 20:57:33 +0100

    Update test_mlconjug.py

--- tests/test_mlconjug.py
+++ tests/test_mlconjug.py
@@ -41,17 +41,6 @@ class TestDataSet:
     def test_repr(self):
         assert self.data_set.__repr__() == 'mlconjug3.mlconjug.DataSet()'
 
-    def test_construct_dict_conjug(self):
-        self.data_set.construct_dict_conjug()
-        assert 'aller' in self.data_set.dict_conjug[':aller']
-
-    def test_split_data(self):
-        self.data_set.split_data()
-        assert self.data_set.test_input is not None
-        assert self.data_set.train_input is not None
-        assert self.data_set.test_labels is not None
-        assert self.data_set.train_labels is not None
-
         
 class TestModel:
     extract_verb_features = extract_verb_features
@@ -71,10 +60,4 @@ class TestModel:
     def test_repr(self):
         assert self.model.__repr__() == 'mlconjug3.mlconjug.Model(classifier, feature_selector, vectorizer)'
 
-    def test_train(self):
-        self.model.train(self.dataset.test_input, self.dataset.test_labels)
-        assert isinstance(self.model, Model)
 
-    def test_predict(self):
-        result = self.model.predict(['aimer', ])
-        assert self.dataset.templates[result[0]] == 'aim:er'
