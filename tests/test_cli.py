e195ff4f63c2f1290cd215b4e1348a8705e4b1ed
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-18 13:26:39 +0100

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,24 +4,35 @@ import logging
 import sys
 from mlconjug3 import *
 
-def test_main(mocker):
-    mock_conjugator = mocker.Mock(spec=Conjugator)
-    mock_conjugator.conjugate.return_value = 'conjugated_verb'
-    mocker.patch('conjugator.Conjugator', return_value=mock_conjugator)
-    mock_open = mocker.mock_open()
-    mocker.patch('builtins.open', mock_open)
+class TestCLI:
+    verbiste = Verbiste(language='fr')
+    conjugator = Conjugator()
 
-    # create a test CLI runner
-    runner = click.testing.CliRunner()
-    # invoke the command with different input
-    result = runner.invoke(main, ['verb'], obj={})
-    assert result.exit_code == 0
-    assert result.output == "conjugated_verb\n"
-    result = runner.invoke(main, ['verb', '-l', 'en'], obj={})
-    assert result.exit_code == 0
-    assert result.output == "conjugated_verb\n"
-    result = runner.invoke(main, ['verb', '-o', 'output.json'], obj={})
-    assert result.exit_code == 0
-    assert result.output == "The conjugations have been succesfully saved to output.json.\n"
-    assert mock_open.called
-    mock_open().write.assert_called_once_with('{"verb": "conjugated_verb"}')
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        verb = 'aller'
+        runner = CliRunner()
+        result = runner.invoke(cli.main, [verb])
+        assert result.exit_code == 0
+        assert 'allassions' in result.output
+        help_result = runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        # assert 'Console script for mlconjug3.' in help_result.output
+
+    @pytest.mark.skipif('3.5' in sys.version,
+                        reason="Random TypeError('invalid file') on Python 3.5.")
+    def test_save_file(self, tmpdir):
+        """
+        Tests file saving feature.
+        """
+        test_verb = self.conjugator.conjugate('aller')
+        path = tmpdir.mkdir("sub").join('conjugations.json')
+        verb = 'aller'
+        runner = CliRunner()
+        result = runner.invoke(cli.main, [verb, '-o', path])
+        assert result.exit_code == 0
+        my_file = Path(path)
+        assert my_file.is_file()
+        with open(my_file, encoding='utf-8') as file:
+            output = json.load(file)
+        assert output['aller'] == test_verb.conjug_info
