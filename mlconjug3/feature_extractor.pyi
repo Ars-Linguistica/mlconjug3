26dfaf16e36711b67911312e9ceac6e3291c771f
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-22 14:55:16 +0100

    Create feature_extractor.pyi

---
+++ mlconjug3/feature_extractor.pyi
@@ -0,0 +1,7 @@
+from typing import Sequence, Mapping, Dict, Tuple, Optional, Union, Set, TextIO
+
+def extract_verb_features(verb: str,
+                              lang: str,
+                              ngram_range: Tuple[int, int]
+                              ) -> Sequence[str]: ...
+  
