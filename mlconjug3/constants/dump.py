e443761f5ad6a799151130ac0a864db09fde232a
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-22 23:20:05 +0100

    Create dump.py

---
+++ mlconjug3/constants/dump.py
@@ -0,0 +1,12 @@
+import .constants
+import tomlkit
+
+def save_to_toml(file_path):
+    data = {}
+    for var in vars(constants):
+        data[var] = vars(constants)[var]
+    toml_str = tomlkit.dumps(data)
+    with open(file_path, "w") as f:
+        f.write(toml_str)
+
+save_to_toml("constants.toml")
