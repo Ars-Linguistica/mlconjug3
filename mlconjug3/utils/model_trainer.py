3348fced4c5f9ba65ae7e926c8c2bcf7436bdb33
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-23 23:31:59 +0100

    Update model_trainer.py

--- mlconjug3/utils/model_trainer.py
+++ mlconjug3/utils/model_trainer.py
@@ -5,83 +5,57 @@ import numpy as np
 from functools import partial
 from time import time
 
-np.random.seed(42)
+class ConjugatorTrainer:
+    def __init__(self, lang, output_folder, split_proportion, feature_extractor, DataSet, Model):
+        self.lang = lang
+        self.output_folder = output_folder
+        self.split_proportion = split_proportion
+        self.feature_extractor = feature_extractor
+        self.DataSet = DataSet
+        self.Model = Model
+        return
 
-# Set languages to train the Conjugator on
-langs = ("ro", "it", "en", "es", "fr", "pt")
+    def train(self):
+        np.random.seed(42)
 
-# Initialize Data Sets
-datasets = {
-    lang: mlconjug3.DataSet(
-        mlconjug3.Verbiste(language=lang).verbs,
-        feature_extractor=mlconjug3.CountVectorizer(
-            analyzer=partial(
-                mlconjug3.extract_verb_features, lang=lang, ngram_range=(2, 7)
-            ),
-            binary=True,
-        ),
-    )
-    for lang in langs
-}
-for dataset in datasets.values():
-    dataset.split_data(proportion=1)
+        # Initialize Data Set
+        dataset = self.DataSet(
+            mlconjug3.Verbiste(language=self.lang).verbs,
+            feature_extractor=self.feature_extractor,
+        )
+        dataset.split_data(proportion=self.split_proportion)
 
-# Initialize Conjugators
-conjugators = {
-    lang: mlconjug3.Conjugator(
-        lang,
-        feature_extractor=datasets[lang].feature_extractor,
-        model=mlconjug3.Model(
-            mlconjug3.SelectFromModel(
-                mlconjug3.LinearSVC(penalty="l1", max_iter=12000, dual=False, verbose=0)
-            ),
-            mlconjug3.SGDClassifier(
-                loss="log",
-                penalty="elasticnet",
-                l1_ratio=0.15,
-                max_iter=40000,
-                alpha=1e-5,
-                verbose=0,
-            ),
-        ),
-    )
-    for lang in langs
-}
+        # Initialize Conjugator
+        conjugator = mlconjug3.Conjugator(
+            self.lang,
+            feature_extractor=dataset.feature_extractor,
+            model=self.Model,
+        )
 
-# Train Conjugators with multiprocessing
-with multiprocessing.Pool() as pool:
-    results = pool.starmap(
-        lambda conjugator, dataset: conjugator.model.train(
-            dataset.verbs_list, dataset.templates_list
-        ),
-        [(conjugators[lang], datasets[lang]) for lang in langs],
-    )
+        # Train Conjugator
+        result = conjugator.model.train(dataset.verbs_list, dataset.templates_list)
 
-# Print training duration
-for lang, result in zip(langs, results):
-    print(f"{lang} model trained on full data set in {result} seconds.")
+        # Print training duration
+        print(f"{self.lang} model trained on full data set in {result} seconds.")
+        return
 
-# Make predictions
-predictions = {
-    lang: conjugators[lang].model.predict(datasets[lang].verbs_list) for lang in langs
-}
+    def predict(self, verbs_list):
+        return self.conjugator.model.predict(verbs_list)
 
-# Assess the performance of the model's predictions
-for lang, prediction in predictions.items():
-    score = len(
-        [a == b for a, b in zip(prediction, datasets[lang].templates_list) if a == b]
-    ) / len(prediction)
-    misses = len(
-        [a != b for a, b in zip(prediction, datasets[lang].templates_list) if a != b]
-    )
-    entries = len(prediction)
-    print(
-        f"The score of the {lang} model is {score} with {misses} misses out of {entries} entries."
-    )
+    def evaluate(self, predictions, templates_list):
+        score = len(
+            [a == b for a, b in zip(predictions, templates_list) if a == b]
+        ) / len(predictions)
+        misses = len(
+            [a != b for a, b in zip(predictions, templates_list) if a != b]
+        )
+        entries = len(predictions)
+        print(
+            f"The score of the {self.lang} model is {score} with {misses} misses out of {entries} entries."
+        )
+        return
 
-for lang in langs:
-    # Save trained model
-    with open(
-        "raw_data/experiments/trained_model-{0}-final.pickle".format(lang), "wb"
-    ) as file:
-        pickle.dump(conjugator.model, file)
+    def save(self):
+        with open(f"{self.output_folder}/trained_model-{self.lang}.pickle", "wb") as file:
+            pickle.dump(self.conjugator.model, file)
+        return
