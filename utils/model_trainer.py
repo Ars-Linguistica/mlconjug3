a30c2616bee8e7fe2dba04b19e4d143de02297f8
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-24 14:37:16 +0100

    Update model_trainer.py

--- utils/model_trainer.py
+++ utils/model_trainer.py
@@ -1,13 +1,19 @@
 import mlconjug3
+from mlconjug.feature_extractor import extract_verv_features
 from functools import partial
 
-params = {'lang': "fr", 
+lang = "fr"
+
+params = {'lang': lang,
           'output_folder': "models", 
           'split_proportion': 0.8,
           'dataset': mlconjug3.DataSet, 
           'model': mlconjug3.Model(
-              mlconjug3.SelectFromModel(mlconjug3.LinearSVC(penalty = "l1", max_iter = 12000, dual = False, verbose = 0)), 
-              mlconjug3.SGDClassifier(loss = "log", penalty = "elasticnet", l1_ratio = 0.15, max_iter = 40000, alpha = 1e-5, verbose = 0)
+              language=lang,
+              vectorizer=CountVectorizer(analyzer=partial(extract_verb_features, lang=lang, ngram_range=(2, 7)),
+                                         binary=True, lowercase=False)
+              feature_selector=mlconjug3.SelectFromModel(mlconjug3.LinearSVC(penalty = "l1", max_iter = 12000, dual = False, verbose = 0)), 
+              classifier=mlconjug3.SGDClassifier(loss = "log", penalty = "elasticnet", l1_ratio = 0.15, max_iter = 40000, alpha = 1e-5, verbose = 0)
           )
          }
 
