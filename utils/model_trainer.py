9aa34bb30907a4567af646921649e5e59762ab0e
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-24 16:03:26 +0100

    Update model_trainer.py

--- utils/model_trainer.py
+++ utils/model_trainer.py
@@ -4,12 +4,10 @@ from functools import partial
 
 lang = "fr"
 
-verbiste = mlconjug3.Verbiste(lang)
-
 params = {'lang': lang,
           'output_folder': "models", 
           'split_proportion': 0.8,
-          'dataset': mlconjug3.DataSet(verbiste.verbs), 
+          'dataset': mlconjug3.DataSet(mlconjug3.Verbiste(lang).verbs), 
           'model': mlconjug3.Model(
               language=lang,
               vectorizer=mlconjug3.CountVectorizer(analyzer=partial(extract_verb_features, lang=lang, ngram_range=(2, 7)),
