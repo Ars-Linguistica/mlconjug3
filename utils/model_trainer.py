27c04c9f48192be7a7ac8f5175cbfdef3c2275e6
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-24 14:15:56 +0100

    Update model_trainer.py

--- utils/model_trainer.py
+++ utils/model_trainer.py
@@ -3,8 +3,7 @@ from functools import partial
 
 params = {'lang': "fr", 
           'output_folder': "models", 
-          'split_proportion': 0.8, 
-          'feature_extractor': mlconjug3.CountVectorizer(analyzer=partial(mlconjug3.extract_verb_features, lang= "fr", ngram_range=(2, 7)), binary=True), 
+          'split_proportion': 0.8,
           'dataset': mlconjug3.DataSet, 
           'model': mlconjug3.Model(
               mlconjug3.SelectFromModel(mlconjug3.LinearSVC(penalty = "l1", max_iter = 12000, dual = False, verbose = 0)), 
