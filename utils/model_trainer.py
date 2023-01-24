f066c7bcae6833cc103f6f1b8ff88bc2d9be1778
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-24 12:00:49 +0100

    Update model_trainer.py

--- utils/model_trainer.py
+++ utils/model_trainer.py
@@ -1,17 +1,19 @@
 import mlconjug3
 from functools import partial
 
-ct = ConjugatorTrainer(
-    lang = "fr", 
-    output_folder = "raw_data/experiments", 
-    split_proportion = 1, 
-    feature_extractor = mlconjug3.CountVectorizer(analyzer=partial(mlconjug3.extract_verb_features, lang= "fr", ngram_range=(2, 7)), binary=True), 
-    DataSet = mlconjug3.DataSet, 
-    Model = mlconjug3.Model(
-        mlconjug3.SelectFromModel(mlconjug3.LinearSVC(penalty = "l1", max_iter = 12000, dual = False, verbose = 0)), 
-        mlconjug3.SGDClassifier(loss = "log", penalty = "elasticnet", l1_ratio = 0.15, max_iter = 40000, alpha = 1e-5, verbose = 0)
-    )
-)
+params = {lang: "fr", 
+          output_folder: "models", 
+          split_proportion: 0.8, 
+          feature_extractor: mlconjug3.CountVectorizer(analyzer=partial(mlconjug3.extract_verb_features, lang= "fr", ngram_range=(2, 7)), binary=True), 
+          dataset: mlconjug3.DataSet, 
+          model: mlconjug3.Model(
+              mlconjug3.SelectFromModel(mlconjug3.LinearSVC(penalty = "l1", max_iter = 12000, dual = False, verbose = 0)), 
+              mlconjug3.SGDClassifier(loss = "log", penalty = "elasticnet", l1_ratio = 0.15, max_iter = 40000, alpha = 1e-5, verbose = 0)
+          )
+         }
+
+ct = ConjugatorTrainer(**params)
+
 ct.train()
 ct.predict()
 ct.evaluate()
