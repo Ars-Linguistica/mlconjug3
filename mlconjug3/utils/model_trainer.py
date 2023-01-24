e93590a6477a093ade6a8b4a2b66a21dc236fc44
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-24 10:26:13 +0100

    Update docstrings

--- mlconjug3/utils/model_trainer.py
+++ mlconjug3/utils/model_trainer.py
@@ -1,3 +1,12 @@
+"""
+This module provides the ConjugatorTrainer class, a tool for training, evaluating,
+and saving models for conjugating verbs in different languages.
+
+The ConjugatorTrainer class allows the user to train a model for a specific language using the mlconjug3 library.
+The user can also evaluate the model's performance and save the trained model for later use.
+
+"""
+
 import multiprocessing
 import mlconjug3
 import pickle
@@ -6,6 +15,17 @@ from functools import partial
 from time import time
 
 class ConjugatorTrainer:
+    """
+    Initialize a ConjugatorTrainer instance.
+
+    Args:
+        lang (str): The language for which the model will be trained.
+        output_folder (str): The directory where the trained model will be saved.
+        split_proportion (float): The proportion of the data set to use for training.
+        feature_extractor (obj): The feature extractor to use for the model.
+        DataSet (class): The DataSet class from the mlconjug3 library.
+        Model (obj): The model to be trained.
+    """
     def __init__(self, lang, output_folder, split_proportion, feature_extractor, DataSet, Model):
         self.lang = lang
         self.output_folder = output_folder
@@ -16,6 +36,9 @@ class ConjugatorTrainer:
         return
 
     def train(self):
+        """
+        Train the model using the specified parameters.
+        """
         np.random.seed(42)
 
         # Initialize Data Set
@@ -40,9 +63,20 @@ class ConjugatorTrainer:
         return
 
     def predict(self):
+         """
+        Make predictions using the trained model.
+
+        Returns:
+            list: A list of predictions for the conjugated verbs.
+        """
         return self.conjugator.model.predict(self.dataset.verbs_list)
 
     def evaluate(self):
+        """
+        Evaluate the performance of the model's predictions.
+
+        Prints the score of the model, with the number of misses out of the total number of entries.
+        """
         predictions = self.conjugator.model.predict(self.dataset.verbs_list)
         score = len(
             [a == b for a, b in zip(predictions, self.dataset.templates_list) if a == b]
@@ -57,6 +91,9 @@ class ConjugatorTrainer:
         return
 
     def save(self):
+        """
+        Save the trained conjugator model to the specified output folder.
+        """
         with open(f"{self.output_folder}/trained_model-{self.lang}.pickle", "wb") as file:
             pickle.dump(self.conjugator.model, file)
         return
