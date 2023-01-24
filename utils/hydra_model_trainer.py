861a080c5dc09e47bfd16d7a1140795a34d1b1b0
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-24 16:18:55 +0100

    Update hydra_model_trainer.py

--- utils/hydra_model_trainer.py
+++ utils/hydra_model_trainer.py
@@ -1,15 +1,31 @@
 import mlconjug3
+from mlconjug3.feature_extractor import extract_verb_features
+from functools import partial
 import hydra
 
 @hydra.main(config_path="config.yaml")
 def main(cfg):
-    ct = ConjugatorTrainer(**cfg)
+    lang = cfg.language
+    params = {'lang': lang,
+              'output_folder': cfg.output_folder,
+              'split_proportion': cfg.split_proportion,
+              'dataset': mlconjug3.DataSet(mlconjug3.Verbiste(lang).verbs),
+              'model': cfg.model
+             }
 
+    ct = mlconjug3.utils.ConjugatorTrainer(**params)
+
+    print("training model...")
     ct.train()
+    print("model has benn trained.")
+
     ct.predict()
+
+    print("evaluating model")
     ct.evaluate()
+
+    print("saving model")
     ct.save()
-    
 
 if __name__ == "__main__":
     main()
