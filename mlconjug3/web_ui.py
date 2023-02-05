21952f175346b3a7eb939f9af4466a424284c49f
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-02-05 01:39:41 +0100

    Update web_ui.py

--- mlconjug3/web_ui.py
+++ mlconjug3/web_ui.py
@@ -1,56 +1,60 @@
-
+from fastapi import FastAPI, Request
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
 from .mlconjug import Conjugator
-
-from fastapi import FastAPI
-from fastapi import UploadFile
-from fastapi import File
-from fastapi import Form
-from fastapi.responses import HTMLResponse
-import htmx
+import json
+import tomlkit
+import yaml
+import logging
+from rich.pretty import pprint, Pretty
+from rich.table import Table
+from rich.columns import Columns
+from rich.console import Console
+import rich
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root(request: Request):
+    return HTMLResponse("""
+    <html>
+        <body>
+            <form action="/result" method="post">
+                <input type="text" name="verb">
+                <input type="submit" value="Conjugate">
+                <br>
+                <label for="language">Language:</label>
+                <select id="language" name="language">
+                  <option value="fr">French</option>
+                  <option value="en">English</option>
+                  <option value="es">Spanish</option>
+                  <option value="it">Italian</option>
+                  <option value="pt">Portuguese</option>
+                  <option value="ro">Romanian</option>
+                </select>
+                <br>
+                <label for="subject">Subject Type:</label>
+                <select id="subject" name="subject">
+                  <option value="abbrev">Abbreviated</option>
+                  <option value="pronoun">Pronoun</option>
+                </select>
+                <br>
+                <input type="submit" value="Save to disk">
+            </form> 
+        </body>
+    </html>
+    """)
 
-@app.get("/conjugate")
-def conjugate(
-    verb: str,
-    language: str = "fr",
-    subject: str = "abbrev",
-    output: str = None,
-    file_format: str = "json",
-    config: UploadFile = File(None)
-):
-    conjugator = Conjugator()
-    conjugator.conjugate(verb, language, subject)
+@app.post("/result")
+async def result(verb: str, language: str = "fr", subject: str = "abbrev"):
+    conjugator = Conjugator(language, subject)
+    conjugation_table = conjugator.conjugate(verb)
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("Tense", style="dim", width=20)
+    table.add_column("Forms", style="dim", width=80)
+    for tense, forms in conjugation_table.items():
+        table.add_row(tense, ", ".join(forms))
+    return HTMLResponse(str(table))
 
-    if file_format == "json":
-        conjugator.to_json(output)
-    elif file_format == "csv":
-        conjugator.to_csv(output)
-
-    table = Table(
-        title="Conjugation Table for {}".format(verb),
-        show_header=True,
-        show_footer=True,
-        header_style="bold green",
-        footer_style="bold red",
-        padding=(0, 1),
-        style="round",
-    )
-
-    table.add_column("Subject")
-    for form in conjugator.forms:
-        table.add_column(form)
-
-    for subject, forms in conjugator.conjugation.items():
-        row = [subject]
-        for form in conjugator.forms:
-            row.append(forms[form])
-        table.add_row(*row)
-
-    html_table = table._repr_html_()
-    return HTMLResponse(content=html_table, status_code=200)
-
+if __name__ == "__main__":
+    app.run()
