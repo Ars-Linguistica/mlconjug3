47673fe3b9a0790bd4aca91f8ff2d8dce06b97a6
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-09-05 15:17:10 +0000

    Updated scikit-learn dependency.

--- mlconjug3/models/models.py
+++ mlconjug3/models/models.py
@@ -52,7 +52,7 @@ class Model:
             )
         if not classifier:
             classifier = SGDClassifier(
-                loss="log_loss",
+                loss="log",
                 penalty="elasticnet",
                 l1_ratio=0.15,
                 max_iter=4000,
