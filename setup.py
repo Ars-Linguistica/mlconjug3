4da5a0a0d5d63020c1d3040215b0c244f861e466
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-09-06 03:17:41 +0000

    Bump version: 3.10.4 → 3.11.0

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup_requirements = [
 
 setup(
     name='mlconjug3',
-    version='3.10.4',
+    version='3.11.0',
     description="A Python library to conjugate French, English, Spanish, Italian, Portuguese and Romanian verbs using Machine Learning techniques.",
     long_description=readme,
     long_description_content_type="text/x-rst",
