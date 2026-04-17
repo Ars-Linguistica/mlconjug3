6c1193190e8bde17ae69bbd71ab91c7ea1506158
Sekou Diao <diao.sekou.nlp@gmail.com>
2026-04-17 11:30:27 +0200

    Removed 'pkg_resource' dependency

--- mlconjug3/constants/constants.py
+++ mlconjug3/constants/constants.py
@@ -1,41 +1,52 @@
-import pkg_resources
+from importlib import resources
 
-import os
-
-from yaml import load, dump
+from yaml import load
 
 try:
-    from yaml import CLoader as Loader, CDumper as Dumper
+    from yaml import CLoader as Loader
 except ImportError:
-    from yaml import Loader, Dumper
-
+    from yaml import Loader
 
 import yaml
 
+
 RESOURCE_PACKAGE = "mlconjug3"
 
-config_file = pkg_resources.resource_filename(RESOURCE_PACKAGE, "config/config.yaml")
-with open(config_file, "r") as stream:
-    try:
-        constants = load(stream, Loader=Loader)
-        ABBREVS = constants["ABBREVS"]
-        ALPHABET = constants["ALPHABET"]
-        AUXILIARIES = constants["AUXILIARIES"]
-        CONJUGATIONS_RESOURCE_PATH = constants["CONJUGATIONS_RESOURCE_PATH"]
-        GENDER = constants["GENDER"]
-        IMPERATIVE_PRONOUNS = constants["IMPERATIVE_PRONOUNS"]
-        LANGUAGE_FULL = constants["LANGUAGE_FULL"]
-        LANGUAGES = constants["LANGUAGES"]
-        NEGATION = constants["NEGATION"]
-        PRE_TRAINED_MODEL_PATH = constants["PRE_TRAINED_MODEL_PATH"]
-        PRONOUNS = constants["PRONOUNS"]
-        SUPPORTED_LANGUAGES = constants["SUPPORTED_LANGUAGES"]
-        RESOURCE_PACKAGE = constants["RESOURCE_PACKAGE"]
-        TRANSLATED_LANGUAGES = constants["TRANSLATED_LANGUAGES"]
-        TRANSLATIONS_PATH = pkg_resources.resource_filename(RESOURCE_PACKAGE, "locale")
-        VERBS_RESOURCE_PATH = constants["VERBS_RESOURCE_PATH"]
-    except yaml.YAMLError as exc:
-        print(exc)
+
+def _load_yaml_resource(package: str, resource: str):
+    """
+    Load a YAML file from package resources in a zip-safe way.
+    """
+    with resources.files(package).joinpath(resource).open("r", encoding="utf-8") as stream:
+        return load(stream, Loader=Loader)
+
+
+# Load config.yaml safely
+constants = _load_yaml_resource(RESOURCE_PACKAGE, "config/config.yaml")
+
+ABBREVS = constants["ABBREVS"]
+ALPHABET = constants["ALPHABET"]
+AUXILIARIES = constants["AUXILIARIES"]
+CONJUGATIONS_RESOURCE_PATH = constants["CONJUGATIONS_RESOURCE_PATH"]
+GENDER = constants["GENDER"]
+IMPERATIVE_PRONOUNS = constants["IMPERATIVE_PRONOUNS"]
+LANGUAGE_FULL = constants["LANGUAGE_FULL"]
+LANGUAGES = constants["LANGUAGES"]
+NEGATION = constants["NEGATION"]
+PRE_TRAINED_MODEL_PATH = constants["PRE_TRAINED_MODEL_PATH"]
+PRONOUNS = constants["PRONOUNS"]
+SUPPORTED_LANGUAGES = constants["SUPPORTED_LANGUAGES"]
+
+# ⚠️ Do NOT overwrite RESOURCE_PACKAGE blindly unless you really intend to
+CONFIG_RESOURCE_PACKAGE = constants.get("RESOURCE_PACKAGE", RESOURCE_PACKAGE)
+
+TRANSLATED_LANGUAGES = constants["TRANSLATED_LANGUAGES"]
+
+# 👇 Export a Traversable instead of a string path
+TRANSLATIONS_RESOURCE = resources.files(CONFIG_RESOURCE_PACKAGE).joinpath("locale")
+
+VERBS_RESOURCE_PATH = constants["VERBS_RESOURCE_PATH"]
+
 
 if __name__ == "__main__":
     pass
